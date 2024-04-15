62523c3ec6434a94b7d78d04169554f385fb243b
Sameh ElGazar <selgazar95@gmail.com>
2024-04-15 03:27:41 +0200

    My Index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,30 +1,34 @@
 #!/usr/bin/python3
-""" Index """
+""" Index module """
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+from models.state import State
 from models.amenity import Amenity
-from models.city import City
 from models.place import Place
 from models.review import Review
-from models.state import State
+from models.city import City
 from models.user import User
-from models import storage
-from api.v1.views import app_views
-from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', methods=['GET'],
+                 strict_slashes=False)
 def status():
-    """ Status of API """
-    return jsonify({"status": "OK"})
+    """
+    Api Status route
+    """
+    return jsonify({'status': 'OK'})
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def number_objects():
-    """ Retrieves the number of each objects by type """
-    classes = [Amenity, City, Place, Review, State, User]
-    names = ["amenities", "cities", "places", "reviews", "states", "users"]
-
-    num_objs = {}
-    for i in range(len(classes)):
-        num_objs[names[i]] = storage.count(classes[i])
-
-    return jsonify(num_objs)
+@app_views.route('/stats', methods=['GET'],
+                 strict_slashes=False)
+def stat():
+    """
+    Api stat route
+    """
+    objs = {'amenities': Amenity, 'cities': City, 'states': State,
+            'places': Place, 'reviews': Review, 'users': User}
+    res = {}
+    for k, v in objs.items():
+        res[k] = storage.count(v)
+    return jsonify(res)
